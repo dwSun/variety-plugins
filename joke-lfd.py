6d1a68cc16404b27dfe884a0311c0eb76c40b686
dwSun <dwSun@Capsule>
2017-05-18 10:42:27 +0800

    add jokes-lfd.py

---
+++ joke-lfd.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from variety.plugins.IQuoteSource import IQuoteSource
+from gettext import gettext as _
+import logging
+
+logger = logging.getLogger("variety")
+
+
+class LocalFilesSource(IQuoteSource):
+    def __init__(self):
+        super(IQuoteSource, self).__init__()
+
+    @classmethod
+    def get_info(cls):
+        return {
+            "name": "joke",
+            "description": _("jokes from laifudao"),
+            "author": "dwSun",
+            "version": "0.1"
+        }
+
+    def supports_search(self):
+        return False
+
+    def activate(self):
+        if self.active:
+            return
+
+        super(LocalFilesSource, self).activate()
+
+        self.quotes = []
+        import requests
+        url = "http://api.laifudao.com/open/xiaohua.json"
+
+        response = requests.request("GET", url)
+        self.quotes = [{'quote': self.rm_br(j['content']),
+                        'author': j['poster'],
+                        'link': j['url'],
+                        'sourceName': j['title']} for j in response.json()]
+
+    def deactivate(self):
+        self.quotes = []
+
+    def rm_br(self, content):
+        while '<br/>' in content:
+            content = content.replace('<br/>', '\n')
+        while '\n\n' in content:
+            content = content.replace('\n\n', '\n')
+        return content
+
+    def get_random(self):
+        return self.quotes
+
+    def get_for_author(self, author):
+        return self.quotes
+
+    def get_for_keyword(self, keyword):
+        return self.quotes
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Variety plugins
+
+tech-reference[http://peterlevi.com/variety/wiki/Plugins]
+
+![logo.png](http://down.laifudao.com/laifudao/www/images/logo.png)jokes source[http://www.laifudao.com/api.asp]
+
+每天开机就能开心一下。
